4b25fbceb7c3103d953368431204667f70c30628
Partha <the.saintneo@gmail.com>
2020-05-28 19:03:30 -0400

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Partha Sarathy on 5/28/20.
+//  Copyright © 2020 Partha Sarathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
